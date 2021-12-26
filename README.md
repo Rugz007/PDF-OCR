b80a66482d360f03dff816614597e5159d766e2a
Rugz007 <dsweb007@gmail.com>
2021-12-26 19:34:56 +0530

    Added multi-page text dumps

---
+++ README.md
@@ -0,0 +1,4 @@
+# PDF OCR using Tesseract
+Use any PDF file, name it as test.pdf and run ocr.py to get output text and bounding boxes for each page.
+
+Made using Tesseract, OpenCV, pdf2image and Python.
--- ocr.py
+++ ocr.py
@@ -5,7 +5,6 @@ import pdf2image
 import os
 
 directory = os.getcwd()+r'\poppler-0.68.0\bin'
-print(directory)
 def convert_pdf_to_image(document, dpi):
     images = []
     images.extend(
@@ -23,10 +22,11 @@ def convert_pdf_to_image(document, dpi):
 def get_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-images = convert_pdf_to_image('test.pdf', 300)
+images = convert_pdf_to_image('test2.pdf', 300)
+string = ''
 for img in images:
     custom_config = r'--oem 3 --psm 6'
-    print(pytesseract.image_to_string(img, config=custom_config))
+    string = string + pytesseract.image_to_string(img, config=custom_config)
     img = get_grayscale(img)
     d = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT)
     n_boxes = len(d['text'])
@@ -37,3 +37,5 @@ for img in images:
 
     cv2.imshow('img', img)
     cv2.waitKey(0)
+with open("Output.txt", "w") as text_file:
+        text_file.write(string)
